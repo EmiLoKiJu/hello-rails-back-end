3ec5221392170c2cace0842672f5a96976bfb112
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-10-24 14:37:09 -0800

    Add database, user, authentication system, and a greeting element

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'greetings/random', to: 'greetings#random'
+  resources :greetings
+  resource :users, only: [:create]
+  post '/login', to: 'users#login'
+  get 'auto_login', to: 'users#auto_login'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(username: 'username1', password: 'password1')
+greeting1 = Greeting.create(text: 'Hello, World!', user_id: 1)
+greeting2 = Greeting.create(text: 'Greetings!', user_id: 1)
+greeting3 = Greeting.create(text: 'Welcome!', user_id: 1)
+greeting4 = Greeting.create(text: 'Hi there!', user_id: 1)
+greeting5 = Greeting.create(text: 'Good day!', user_id: 1)
