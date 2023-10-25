d4682627622af2c5164be14092b6c3ca44ad0b7e
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-10-24 21:07:57 -0800

    Add restriction in cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:5000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post]
   end
 end
